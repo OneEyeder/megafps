87a95cb3250c924337d8a355331fb3c588c366ff
Константин Герц <120933936+OneEyeder@users.noreply.github.com>
2025-01-14 20:43:59 +0300

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    burgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
